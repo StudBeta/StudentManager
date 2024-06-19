aba82ac716e7be10644ad5d3e00581ba24fb927a
Jameel <47351791+jameelappsintegra@users.noreply.github.com>
2024-06-19 15:59:44 +0400

    Update Student.ts

--- src/app/lib/models/Student.ts
+++ src/app/lib/models/Student.ts
@@ -1,20 +1,50 @@
-import mongoose, { Document, Schema } from "mongoose";
+import mongoose, { Document, Schema, Model } from "mongoose";
 
+// Define the interface for the Student document
 export interface IStudent extends Document {
-  studentName: string;
-  dob: string;
+  student_id: number;
+  name: string;
+  age: number;
+  dob: Date;
+  parent_id: number;
+  class_id: number;
+  teacher_id: number;
 }
 
-const schoolSchema: Schema = new mongoose.Schema({
-  studentName: {
+// Define the schema for the Student model
+const studentSchema: Schema = new Schema({
+  student_id: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  name: {
     type: String,
     required: true,
   },
+  age: {
+    type: Number,
+    required: true,
+  },
   dob: {
-    type: String,
+    type: Date,
+    required: true,
+  },
+  parent_id: {
+    type: Number,
+    required: true,
+  },
+  class_id: {
+    type: Number,
+    required: true,
+  },
+  teacher_id: {
+    type: Number,
+    required: true,
   },
 });
 
-const student = mongoose.model<IStudent>("student", studentSchema);
+// Define and export the Student model
+const Student: Model<IStudent> = mongoose.models.Student || mongoose.model<IStudent>("Student", studentSchema);
 
-export default student;
+export default Student;
